df271f94734a71855e87a96cfb76de1c966a54a9
gregdorsey <gregdorsey@roaming-171-25.host.udel.edu>
2016-11-28 22:38:58 -0500

    initial checkin

---
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -0,0 +1,86 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import domain.PersonDomainModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Person_Test {
+		
+	private static PersonDomainModel person1;
+	private static UUID person1UUID = UUID.randomUUID();			
+	
+	@BeforeClass
+	public static void personInstance() throws Exception{
+		
+		Date person1Birth = null;
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		 person1 = new PersonDomainModel();
+		 
+		try {
+			person1Birth = dateFormat.parse("1994-11-27");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		person1.setPersonID(person1UUID);
+		person1.setFirstName("Mingkun");
+		person1.setMiddleName("a");
+		person1.setLastName("Chen");
+		person1.setBirthday(person1Birth);
+		person1.setCity("Elkton");
+		person1.setStreet("702 Stone Gate Blvd");
+		person1.setPostalCode(21921);
+		
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void test_PersonDAL(){
+		PersonDAL x = new PersonDAL();
+		x.addPerson(person1);
+		assertTrue(person1 == x.addPerson(person1));
+		ArrayList<PersonDomainModel> y = new ArrayList<PersonDomainModel>();
+		y.add(person1);
+		assertTrue(y == x.getPersons());
+		x.getPerson(person1UUID);
+		assertTrue(person1 == x.getPerson(person1UUID));
+		y.remove(person1);
+		x.deletePerson(person1UUID);
+		x.getPersons();
+		assertTrue(y == x.getPersons());
+		x.addPerson(person1);
+		person1.setFirstName("John");
+		x.updatePerson(person1);
+		assertTrue(person1 == x.updatePerson(person1));
+	}
+
+}
